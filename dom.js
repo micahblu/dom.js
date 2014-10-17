5571c031217edfcc9fc8ac59ee45dd13e2abb4ab
Micah Blu <micahblu@gmail.com>
2014-10-17 15:52:03 -0700

    Improved nested element creation
    got rid of createTable

--- dom.js
+++ dom.js
@@ -1,8 +1,14 @@
+//isArray Polyfill
+if (!Array.isArray) {
+  Array.isArray = function(arg) {
+    return Object.prototype.toString.call(arg) === '[object Array]';
+  };
+}
 //Factory pattern for creating dom els
 function Dom(){
   var self = this;
   
-  var tags = [ "A", "BASE",  "BASEFONT",  "BIG",  "BLOCKQUOTE",  "BODY",  "BR",  "B",  "CAPTION",  "CENTER",  "CITE",  "CODE",  "DD",  "DFN",  "DIR",  "DIV",  "DL",  "DT",  "EM",  "FONT",  "FORM",  "H1",  "H2",  "H3",  "H4",  "H5",  "H6",  "HEAD",  "HR",  "HTML",  "IMG",  "INPUT",  "ISINDEX",  "I",  "KBD",  "LINK",  "LI",  "MAP",  "MENU",  "META",  "OL",  "OPTION",  "PARAM",  "PRE",  "P",  "SAMP",  "SCRIPT",  "SELECT",  "SMALL",  "STRIKE",  "STRONG",  "STYLE",  "SUB",  "SUP",  "TABLE",  "TD",  "TEXTAREA",  "TH",  "TITLE",  "TR",  "TT",  "UL",  "U",  "VAR"];
+  var tags = [ "A", "BASE",  "BASEFONT",  "BIG",  "BLOCKQUOTE",  "BODY",  "BR",  "B",  "CAPTION",  "CENTER",  "CITE",  "CODE",  "DD",  "DFN",  "DIR",  "DIV",  "DL",  "DT",  "EM",  "FONT",  "FORM",  "H1",  "H2",  "H3",  "H4",  "H5",  "H6",  "HEAD",  "HR",  "HTML",  "IMG",  "INPUT",  "ISINDEX",  "I",  "KBD",  "LINK",  "LI",  "MAP",  "MENU",  "META",  "OL",  "OPTION",  "PARAM",  "PRE",  "P",  "SAMP",  "SCRIPT",  "SELECT",  "SMALL",  "STRIKE",  "STRONG",  "STYLE",  "SUB",  "SUP",  "TABLE",  "TBODY",  "TD",  "TEXTAREA",  "TH", "THEAD",  "TITLE",  "TR",  "TT",  "UL",  "U",  "VAR"];
   
   function cssString(styles){
     var str = '';
@@ -22,8 +28,20 @@ function Dom(){
     return false;
   }
 
+
+  function extend(obj /* , ...source */) {
+    for (var i = 1; i < arguments.length; i++) {
+      for (var key in arguments[i]) {
+        if (Object.prototype.hasOwnProperty.call(arguments[i], key)) {
+          obj[key] = (typeof arguments[i][key] === 'object' ? extend(obj[key], arguments[i][key]) : arguments[i][key]);
+        }
+      }
+    }
+    return obj;
+  }
+ 
   this.create = function(type, props){
-    var el = document.createElement(type), value, child;
+    var el = document.createElement(type), value, child, i, j;
     if(props){
       if(props.styles){
         el.style.cssText = cssString(props.styles);
@@ -33,86 +51,25 @@ function Dom(){
         value = props[prop];
         prop = (prop === "text" ? "innerText" : prop);
         prop = (prop === "html" ? "innerHTML" : prop);
+        
+        //Atts
         el[prop] = value;
 
         if(has(prop.toUpperCase(), tags)){
-          console.log('we have a nested element');
-          child = this.create(prop, value);
-          el.appendChild(child);
+          if(value.nodes){
+            console.log(prop + ' has ' + value.nodes.length + ' nodes');
+            for(i=0, j=value.nodes.length; i<j; i++){
+              
+              child = this.create(prop, extend((value.styles ? {styles: value.styles} : {}), value.nodes[i]));
+              el.appendChild(child);
+            }
+          }else{
+            child = this.create(prop, value);
+            el.appendChild(child);
+          }
         }
       }
     }
     return el;
-  };
-
-  this.createTable = function(props){
-    var theadData = props.thead,
-        tbodyData = props.tbody,
-        tfootData = props.foot || props.footer;
-
-    // remove so they're not added as properties to the actual element when created
-    delete props.thead;
-    delete props.tbody;
-    delete props.footer;
-    delete props.foot;
-
-    var table = this.create("table", props),
-        thead = this.create("thead"),
-        tbody = this.create("tbody"),
-        tfoot = this.create("tfoot"),
-        tr = this.create("tr"),
-        i, j;
-
-    if(theadData && theadData.length > 0){
-      thead.appendChild(tr);
-
-      for(i=0, j = theadData.length; i<j; i++){
-        thead.appendChild(this.create("th", {
-          text: theadData[i],
-          styles: (props.styles.thead && props.styles.thead.th ? props.styles.thead.th : {})
-        }));
-      }
-    }
-
-    if(tbodyData && tbodyData.length > 0){
-      tbody.appendChild(tr);
-      for(i=0, j = tbodyData.length; i<j; i++){
-        tbody.appendChild(this.create("td", {
-          text: tbodyData[i],
-          styles: (props.styles.tbody && props.styles.tbody.td ? props.styles.tbody.td : {})
-        }));
-      }
-    }
-
-    if(tfootData && tfootData.length > 0){
-      tfoot.appendChild(tr);
-      for(i=0, j=tfootData.length; i<j; i++){
-        tfoot.appendChild(this.create("td", {
-          text: tfootData[i]
-        }));
-      }
-    }
-    table.appendChild(thead);
-    table.appendChild(tbody);
-    table.appendChild(tfoot);
-
-    table.populate = function(data){
-      console.log(data);
-      var tr;
-      for(var i=0, j=data.length; i<j; i++){
-        tr = self.create("tr");
-        for(var k=0, l=data[i].length; k<l; k++){
-          tr.appendChild(self.create("td", {
-            text: data[i][k],
-            styles: (props.styles.tbody && props.styles.tbody.td ? props.styles.tbody.td : {})
-          }));
-        }
-      }
-      var tbody = this.getElementsByTagName("tbody")[0];
-      tbody.appendChild(tr);
-
-    };
-
-    return table;
   };
 }
